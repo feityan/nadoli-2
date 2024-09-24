6f8164d64bbc5cbd5088fd1c1bfc81fba1b63134
feityan <fedorkornienko@gmail.com>
2024-09-25 02:22:47 +0400

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,14 +2,11 @@ require("dotenv").config();
 
 const ghostContentAPI = require("@tryghost/content-api");
 
-// Import stripDomain function
-const { stripDomain } = require('../../.eleventy.js');
-
 // Init Ghost API
 const api = new ghostContentAPI({
   url: process.env.GHOST_API_URL,
   key: process.env.GHOST_CONTENT_API_KEY,
-  version: "v5"
+  version: "v2"
 });
 
 // Get all site information
@@ -23,14 +20,6 @@ module.exports = async function() {
     });
 
   if (process.env.SITE_URL) siteData.url = process.env.SITE_URL;
-
-  // Process navigation URLs if they exist
-  if (siteData.navigation) {
-    siteData.navigation = siteData.navigation.map(item => ({
-      ...item,
-      url: stripDomain(item.url)
-    }));
-  }
 
   return siteData;
 };
